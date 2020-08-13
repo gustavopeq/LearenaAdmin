e71d7b628f00a2b0e80de1666162e5d3ce4dddaf
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2020-08-13 11:36:15 -0700

    Signup system created

--- app/src/main/java/gustavo/projects/learenaadmin/signup/SignupFragment.kt
+++ app/src/main/java/gustavo/projects/learenaadmin/signup/SignupFragment.kt
@@ -6,27 +6,62 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navGraphViewModels
 
 import gustavo.projects.learenaadmin.R
 import gustavo.projects.learenaadmin.databinding.SignupFragmentBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class SignupFragment : Fragment() {
 
+    companion object {
+        private val MIN_PASSWORD_LENGTH = 6
+    }
+
+    private lateinit var binding: SignupFragmentBinding
     private lateinit var viewModel: SignupViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<SignupFragmentBinding>(inflater, R.layout.signup_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.signup_fragment, container, false)
 
         viewModel = ViewModelProvider(this).get(SignupViewModel::class.java)
 
-        binding.createBtn.setOnClickListener { viewModel.createUser(binding.emailEditText.text.toString(), binding.passwordEditText.text.toString()) }
+        binding.createBtn.setOnClickListener { validateCreateAccountForm() }
+
+        viewModel.userCreated.observe(viewLifecycleOwner, Observer<Boolean>{userCreated ->
+            if(userCreated) userCreationSuccess() else userCreationFailed()
+        })
 
         return binding.root
+    }
+
+    private fun validateCreateAccountForm() {
+        if( !binding.passwordEditText.text.isNullOrBlank() && binding.passwordEditText.text.length >= MIN_PASSWORD_LENGTH
+            && binding.passwordEditText.text.toString() == binding.confirmPwEditText.text.toString()
+            && !binding.emailEditText.text.isNullOrBlank() && binding.emailEditText.text.contains("@", true)
+            && !binding.firstNameEditText.text.isNullOrBlank() && !binding.lastNameEditText.text.isNullOrBlank()
+        ) {
+            viewModel.createUser(binding.emailEditText.text.toString(), binding.passwordEditText.text.toString())
+        }else{
+            Toast.makeText(context, "Incorrect or missing field!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun userCreationSuccess() {
+        Toast.makeText(context, "New user created!", Toast.LENGTH_LONG).show()
+        findNavController().navigate(R.id.action_signupFragment_to_loginFragment)
+    }
+
+    private fun userCreationFailed() {
+        Toast.makeText(context, "New user couldn't be created...", Toast.LENGTH_LONG).show()
     }
 
 }
--- app/src/main/java/gustavo/projects/learenaadmin/signup/SignupViewModel.kt
+++ app/src/main/java/gustavo/projects/learenaadmin/signup/SignupViewModel.kt
@@ -1,6 +1,8 @@
 package gustavo.projects.learenaadmin.signup
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -10,14 +12,20 @@ class SignupViewModel : ViewModel() {
 
     private var auth: FirebaseAuth = Firebase.auth
 
+    private val _userCreated = MutableLiveData<Boolean>()
+    val userCreated: LiveData<Boolean>
+        get() = _userCreated
+
     fun createUser(email: String, password: String) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener() { task ->
                 if (task.isSuccessful) {
                     Log.d("print", "User registration completed...")
+                    _userCreated.value = true
                     val user = auth.currentUser
                 } else {
                     Log.d("print", "User registration failed")
+                    _userCreated.value = false
                 }
             }
     }
